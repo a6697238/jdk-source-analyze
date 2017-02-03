87d36eccbb90f357bb7c21dc7d11a62431a6b4d2
a6697238 <308788715@qq.com>
2017-02-03 13:00:37 +0800

    修改文件

--- src/test/java/com/houlu/java/test/AppTest.java
+++ src/test/java/com/houlu/java/test/AppTest.java
@@ -9,6 +9,6 @@ import junit.framework.TestSuite;
  */
 public class AppTest {
     public static void main(String[] args) {
-        System.out.println("sd");
+        System.out.println("sdss");
     }
 }
