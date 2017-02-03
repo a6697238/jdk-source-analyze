9a948b6e72cc6f148623107d676f7b11356e8952
a6697238 <308788715@qq.com>
2017-02-03 12:45:53 +0800

    sdasdasdsa

---
+++ src/test/java/com/houlu/java/test/AppTest2.java
@@ -0,0 +1,16 @@
+package com.houlu.java.test;
+
+/**
+ * 类名称: AppTest2 <br>
+ * 类描述: <br>
+ *
+ * @author lu.hou
+ * @version 1.0.0
+ * @since 17/2/3 下午12:45
+ */
+public class AppTest2 {
+
+    public static void main(String[] args) {
+        System.out.println("ceshi002");
+    }
+}
