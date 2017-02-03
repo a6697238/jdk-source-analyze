cecd3ccb2241f245b69f5fca998f000fd40287af
a6697238 <308788715@qq.com>
2017-02-03 15:26:45 +0800

    tijiaoceshi

--- src/test/java/com/houlu/java/test/AppTest.java
+++ src/test/java/com/houlu/java/test/AppTest.java
@@ -6,7 +6,7 @@ package com.houlu.java.test;
  */
 public class AppTest {
     public static void main(String[] args) {
-        System.out.println("sdsss");
-         System.out.println("sdsss");
+        System.out.println("testApp");
+        System.out.println("testApp2");
     }
 }
