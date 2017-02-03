f6e4b4345842a4c23edf63c35df226c3149cb64b
a6697238 <308788715@qq.com>
2017-02-03 13:41:38 +0800

    修改文件

--- src/test/java/com/houlu/java/test/AppTest.java
+++ src/test/java/com/houlu/java/test/AppTest.java
@@ -6,6 +6,6 @@ package com.houlu.java.test;
  */
 public class AppTest {
     public static void main(String[] args) {
-        System.out.println("sdss");
+        System.out.println("sdsss");
     }
 }
